9fd767a149ed20a72e5f0cea41699fab6829f2b5
Samuel Zwinglio <samuelzwinglio@gmail.com>
2026-07-04 02:18:00 -0300

    [FAK-10] - Add favicon

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: '172.26.178.48',
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      host: '172.26.178.48',
+    },
     proxy: {
       "/api": {
         target: `http://localhost:${process.env.PORT || 3001}`,
